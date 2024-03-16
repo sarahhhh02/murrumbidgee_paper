042d13a488b489a29231fa77eec627fa49e082c8
Sarah Lee <sarahhhh.lee@mail.utoronto.ca>
2024-03-16 17:11:09 -0400

    update scripts/03-test_data.R

--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -11,11 +11,24 @@
 library(tidyverse)
 
 #Load package
+homicides_clean1 <- read_csv("data/analysis_data/homicides_clean1.csv")
 homicides_clean2 <- read_csv("data/analysis_data/homicides_clean2.csv")
+homicides_clean3 <- read_csv("data/analysis_data/homicides_clean3.csv")
 
 #### Test data ####
 #Test coloumn class of variables; should all return TRUE
+#homicides_clean1
+is.numeric(homicides_clean1$year)
+is.character(homicides_clean1$homicide_type)
+is.character(homicides_clean1$day)
+
+#homicides_clean2
 is.numeric(homicides_clean2$year)
 is.character(homicides_clean2$homicide_type)
-is.numeric(homicides_clean2$count)
+is.numeric(homicides_clean2$count_type)
+
+#homicides_clean3
+is.numeric(homicides_clean3$year)
+is.character(homicides_clean3$day)
+is.numeric(homicides_clean3$count_day)
 
