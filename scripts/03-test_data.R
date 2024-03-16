9be9176c7f2ec9d76b180e2f6ab7c5476b136904
Sarah Lee <sarahhhh.lee@mail.utoronto.ca>
2024-03-16 01:24:23 -0400

    update scripts/03-test_data.R

--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -1,15 +1,23 @@
 #### Preamble ####
-# Purpose: Tests... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Tests data on Annual Toronto Homicides cleaned data
+# Author: Sarah Lee
+# Date: 15 March 2024 
+# Contact: sarahhhh.lee@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Any other information needed? Data from data/analysis_data/homicides_clean.csv
 
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
+
+#Load package
+homicides_clean <- read_csv("data/analysis_data/homicides_clean.csv")
 
 #### Test data ####
+#Test coloumn class of variables; should all return TRUE
+is.numeric(homicides_clean$year)
+is.character(homicides_clean$month)
+is.numeric(homicides_clean$day)
+is.character(homicides_clean$day_of_week)
+is.character(homicides_clean$homicide_type)
+
