21069cbb65a25f9945db797f5f1737fd8fec2c48
Sarah Lee <sarahhhh.lee@mail.utoronto.ca>
2024-03-16 02:12:09 -0400

    update scripts/04-model.R

--- scripts/04-model.R
+++ scripts/04-model.R
@@ -1,8 +1,8 @@
 #### Preamble ####
 # Purpose: Models... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Author: Sarah Lee
+# Date: 15 March 2024 
+# Contact: sarahhhh.lee@mail.utoronto.ca
 # License: MIT
 # Pre-requisites: [...UPDATE THIS...]
 # Any other information needed? [...UPDATE THIS...]
@@ -13,20 +13,20 @@ library(tidyverse)
 library(rstanarm)
 
 #### Read data ####
-analysis_data <- read_csv("data/analysis_data/analysis_data.csv")
+homicides_clean <- read_csv("data/analysis_data/homicides_clean.csv")
 
 ### Model data ####
+
 first_model <-
   stan_glm(
-    formula = flying_time ~ length + width,
-    data = analysis_data,
+    formula = year ~ homicide_type,
+    data = homicides_clean,
     family = gaussian(),
     prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
     prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
     prior_aux = exponential(rate = 1, autoscale = TRUE),
-    seed = 853
+    seed = 1168
   )
-
 
 #### Save model ####
 saveRDS(
